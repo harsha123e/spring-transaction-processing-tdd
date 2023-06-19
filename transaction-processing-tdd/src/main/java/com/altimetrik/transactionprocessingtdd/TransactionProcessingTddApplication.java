bf8e758e57cb50d16a8a580e3f88af4722b7e3d2
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-20 03:59:09 +0530

    Add project setup files

---
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/TransactionProcessingTddApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.transactionprocessingtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionProcessingTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionProcessingTddApplication.class, args);
+	}
+
+}
