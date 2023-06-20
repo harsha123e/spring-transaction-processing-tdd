3173b5100e943a1b79e801996b798af415ff513c
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-20 09:58:37 +0530

    Update controller to handle exceptions

--- transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/controller/TransactionController.java
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/controller/TransactionController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.altimetrik.transactionprocessing.utils.TransactionProcessingConstants;
+import com.altimetrik.transactionprocessingtdd.exception.FileProcessingException;
+import com.altimetrik.transactionprocessingtdd.exception.InvalidFileNameException;
+import com.altimetrik.transactionprocessingtdd.exception.UnsupportedFileFormatException;
 import com.altimetrik.transactionprocessingtdd.service.TransactionService;
 
 @RestController
@@ -21,21 +25,27 @@ public class TransactionController {
 	@PostMapping("/upload")
 	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
 		if (file.isEmpty()) {
-			return ResponseEntity.badRequest().body("File is empty");
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.FILE_IS_EMPTY);
 		}
 
 		String originalFilename = file.getOriginalFilename();
 		if (originalFilename != null && !isValidFileType(originalFilename)) {
-			return ResponseEntity.badRequest().body("Invalid file type");
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.UNSUPPORTED_FILE_FORMAT);
 		}
 
 		try {
-			transactionService.processAndSaveTransactions(file.getBytes());
+			transactionService.processAndSaveTransactions(file);
+		} catch (InvalidFileNameException e) {
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.UNSUPPORTED_FILE_FORMAT);
+		} catch (UnsupportedFileFormatException e) {
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.UNSUPPORTED_FILE_FORMAT);
+		} catch (FileProcessingException e) {
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.FILE_PROCESSING_ERROR);
 		} catch (IOException e) {
-			return ResponseEntity.badRequest().body("Unable to access file");
+			return ResponseEntity.badRequest().body(TransactionProcessingConstants.FILE_PROCESSING_ERROR);
 		}
 
-		return ResponseEntity.ok("File uploaded successfully");
+		return ResponseEntity.ok(TransactionProcessingConstants.FILE_UPLOAD_SUCCESSFUL);
 	}
 
 	private boolean isValidFileType(String fileName) {
