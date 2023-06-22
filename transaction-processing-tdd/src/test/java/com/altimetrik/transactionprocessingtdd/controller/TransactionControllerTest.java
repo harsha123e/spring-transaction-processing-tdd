f347bb49052dd6f4400de913d056a1af44404aff
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-22 15:12:23 +0530

    Update test cases to use constants, use doThrow, inline local variables

--- transaction-processing-tdd/src/test/java/com/altimetrik/transactionprocessingtdd/controller/TransactionControllerTest.java
+++ transaction-processing-tdd/src/test/java/com/altimetrik/transactionprocessingtdd/controller/TransactionControllerTest.java
@@ -1,12 +1,11 @@
 package com.altimetrik.transactionprocessingtdd.controller;
 
+import static com.altimetrik.transactionprocessingtdd.utils.TestTransactionProcessingConstants.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doThrow;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.DisplayName;
@@ -18,10 +17,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
 
+import com.altimetrik.transactionprocessingtdd.exception.EmptyFileException;
+import com.altimetrik.transactionprocessingtdd.exception.UnsupportedFileFormatException;
 import com.altimetrik.transactionprocessingtdd.service.TransactionService;
-import com.altimetrik.transactionprocessingtdd.utils.TestTransactionProcessingConstants;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionControllerTest {
@@ -34,106 +33,89 @@ class TransactionControllerTest {
 
 	@Test
 	@DisplayName("Valid csv file upload")
-	public void testUploadFile_WithValidCSVFile_ReturnsSuccessResponse() throws Exception {
-		String filePath = TestTransactionProcessingConstants.CSV_FILE_RESOURCE;
+	public void testUploadFile_WithValidCSVFile_ReturnsSuccessResponse() throws IOException {
 
-		String controllerParam = "file";
-		String originalFileName = "transactions.csv";
-		String contentType = "text/csv";
-		Path path = Paths.get(filePath);
-		byte[] fileContent = Files.readAllBytes(path);
-		MockMultipartFile file = new MockMultipartFile(controllerParam, originalFileName, contentType, fileContent);
+		byte[] fileContent = Files.readAllBytes(Paths.get(CSV_FILE_RESOURCE));
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, CSV_FILE_NAME, CONTENT_TYPE_CSV, fileContent);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertEquals(TestTransactionProcessingConstants.FILE_UPLOAD_SUCCESSFUL, response.getBody());
+		assertEquals(FILE_UPLOAD_SUCCESSFUL, response.getBody());
 
 	}
 
 	@Test
 	@DisplayName("Valid excel file upload")
 	public void testUploadFile_WithValidExcelFile_ReturnsSuccessResponse() throws Exception {
-		String filePath = TestTransactionProcessingConstants.EXCEL_FILE_RESOURCE;
 
-		String controllerParam = "file";
-		String originalFileName = "transactions.xlsx";
-		String contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-		Path path = Paths.get(filePath);
-		byte[] fileContent = Files.readAllBytes(path);
-		MockMultipartFile file = new MockMultipartFile(controllerParam, originalFileName, contentType, fileContent);
+		byte[] fileContent = Files.readAllBytes(Paths.get(EXCEL_FILE_RESOURCE));
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, EXCEL_FILE_NAME, CONTENT_TYPE_EXCEL, fileContent);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertEquals(TestTransactionProcessingConstants.FILE_UPLOAD_SUCCESSFUL, response.getBody());
+		assertEquals(FILE_UPLOAD_SUCCESSFUL, response.getBody());
 
 	}
 
 	@Test
 	@DisplayName("Valid text file upload")
 	public void testUploadFile_WithValidTextFile_ReturnsSuccessResponse() throws Exception {
-		String filePath = TestTransactionProcessingConstants.TEXT_FILE_RESOURCE;
 
-		String controllerParam = "file";
-		String originalFileName = "transactions.txt";
-		String contentType = "text/plain;charset=UTF-8";
-		Path path = Paths.get(filePath);
-		byte[] fileContent = Files.readAllBytes(path);
-		MockMultipartFile file = new MockMultipartFile(controllerParam, originalFileName, contentType, fileContent);
+		byte[] fileContent = Files.readAllBytes(Paths.get(TEXT_FILE_RESOURCE));
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, TEXT_FILE_NAME, CONTENT_TYPE_TXT, fileContent);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.OK, response.getStatusCode());
-		assertEquals(TestTransactionProcessingConstants.FILE_UPLOAD_SUCCESSFUL, response.getBody());
+		assertEquals(FILE_UPLOAD_SUCCESSFUL, response.getBody());
 
 	}
 
 	@Test
 	@DisplayName("Invalid file upload")
 	public void testUploadFile_WithInvalidFileType_ReturnsBadRequest() throws Exception {
-		String filePath = TestTransactionProcessingConstants.TEST_FILE_RESOURCES + "/invalid_file.pdf";
 
-		String controllerParam = "file";
-		String originalFileName = "invalid_file.pdf";
-		String contentType = "application/pdf"; // Invalid file type
-		Path path = Paths.get(filePath);
-		byte[] fileContent = Files.readAllBytes(path);
-		MockMultipartFile file = new MockMultipartFile(controllerParam, originalFileName, contentType, fileContent);
+		byte[] fileContent = Files.readAllBytes(Paths.get(INVALID_FILE_RESOURCE));
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, INVALID_FILE_NAME, CONTENT_TYPE_PDF, fileContent);
+		doThrow(new UnsupportedFileFormatException(UNSUPPORTED_FILE_FORMAT)).when(transactionService)
+				.processAndSaveTransactions(file);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+		assertEquals(UNSUPPORTED_FILE_FORMAT, response.getBody());
 
 	}
 
 	@Test
 	@DisplayName("Empty file upload")
 	public void testUploadFile_WithEmptyFile_ReturnsBadRequest() throws Exception {
+
 		byte[] emptyFileContent = new byte[0]; // Empty file content
-		MockMultipartFile file = new MockMultipartFile("file", "empty.csv", "text/csv", emptyFileContent);
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, CSV_FILE_NAME, CONTENT_TYPE_CSV, emptyFileContent);
+		doThrow(new EmptyFileException(FILE_IS_EMPTY)).when(transactionService).processAndSaveTransactions(file);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-		assertEquals(TestTransactionProcessingConstants.FILE_IS_EMPTY, response.getBody());
+		assertEquals(FILE_IS_EMPTY, response.getBody());
 
 	}
 
 	@Test
 	@DisplayName("IOException when processing file")
 	public void testUploadFile_WithIOException_ReturnsBadRequest() throws Exception {
-		MockMultipartFile file = new MockMultipartFile("file", "transactions.csv", "text/csv",
-				"file content".getBytes());
 
-		doAnswer(invocation -> {
-			throw new IOException(TestTransactionProcessingConstants.FILE_PROCESSING_ERROR);
-		}).when(transactionService).processAndSaveTransactions(any(MultipartFile.class));
+		byte[] fileContent = "file content".getBytes();
+		MockMultipartFile file = new MockMultipartFile(FILE_PARAM, CSV_FILE_NAME, CONTENT_TYPE_CSV, fileContent);
+		doThrow(new IOException(FILE_PROCESSING_ERROR)).when(transactionService).processAndSaveTransactions(file);
 
 		ResponseEntity<String> response = transactionController.uploadFile(file);
 
 		assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-		assertEquals(TestTransactionProcessingConstants.FILE_PROCESSING_ERROR, response.getBody());
+		assertEquals(FILE_PROCESSING_ERROR, response.getBody());
 	}
 
 }
