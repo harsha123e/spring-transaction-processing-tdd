b5198fc65140aab9aa9f3e2c74889a070243fef3
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-22 15:39:07 +0530

    Add empty file exception, remove invalid file name exception

---
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/exception/EmptyFileException.java
@@ -0,0 +1,13 @@
+package com.altimetrik.transactionprocessingtdd.exception;
+
+public class EmptyFileException extends Exception {
+
+	public EmptyFileException() {
+		super();
+	}
+
+	public EmptyFileException(String message) {
+		super(message);
+	}
+
+}
