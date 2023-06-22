e68e07b9fec3e0113c9a1c7fc6e3a82aa34deab0
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-22 15:39:52 +0530

    Update to not validate file name at controller level

--- transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/controller/TransactionController.java
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/controller/TransactionController.java
@@ -1,7 +1,8 @@
 package com.altimetrik.transactionprocessingtdd.controller;
 
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.FILE_UPLOAD_SUCCESSFUL;
+
 import java.io.IOException;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +11,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.altimetrik.transactionprocessingtdd.exception.EmptyFileException;
 import com.altimetrik.transactionprocessingtdd.exception.FileProcessingException;
-import com.altimetrik.transactionprocessingtdd.exception.InvalidFileNameException;
 import com.altimetrik.transactionprocessingtdd.exception.UnsupportedFileFormatException;
 import com.altimetrik.transactionprocessingtdd.service.TransactionService;
-import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.*;
 
 @RestController
 public class TransactionController {
@@ -24,33 +24,14 @@ public class TransactionController {
 
 	@PostMapping("/upload")
 	public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-		if (file.isEmpty()) {
-			return ResponseEntity.badRequest().body(FILE_IS_EMPTY);
-		}
-
-		String originalFilename = file.getOriginalFilename();
-		if (originalFilename != null && !isValidFileType(originalFilename)) {
-			return ResponseEntity.badRequest().body(UNSUPPORTED_FILE_FORMAT);
-		}
 
 		try {
 			transactionService.processAndSaveTransactions(file);
-		} catch (InvalidFileNameException e) {
-			return ResponseEntity.badRequest().body(UNSUPPORTED_FILE_FORMAT);
-		} catch (UnsupportedFileFormatException e) {
-			return ResponseEntity.badRequest().body(UNSUPPORTED_FILE_FORMAT);
-		} catch (FileProcessingException e) {
-			return ResponseEntity.badRequest().body(FILE_PROCESSING_ERROR);
-		} catch (IOException e) {
-			return ResponseEntity.badRequest().body(FILE_PROCESSING_ERROR);
+		} catch (EmptyFileException | UnsupportedFileFormatException | FileProcessingException | IOException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 
 		return ResponseEntity.ok(FILE_UPLOAD_SUCCESSFUL);
-	}
-
-	private boolean isValidFileType(String fileName) {
-		String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
-		return Set.of("csv", "xls", "xlsx", "txt").contains(fileExtension);
 	}
 
 }
