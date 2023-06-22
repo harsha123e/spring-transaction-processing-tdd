b0955be05dcfba4defeca7c4a88e0ee57dc39bf1
snavundru <snavundru@AIPL7036LT14898.altimetrik.com>
2023-06-22 17:35:50 +0530

    Refactor code to use functional programming

--- transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/service/TransactionService.java
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/service/TransactionService.java
@@ -4,13 +4,12 @@ import java.io.IOException;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import com.altimetrik.transactionprocessingtdd.exception.FileProcessingException;
-import com.altimetrik.transactionprocessingtdd.exception.InvalidFileNameException;
+import com.altimetrik.transactionprocessingtdd.exception.EmptyFileException;
 import com.altimetrik.transactionprocessingtdd.exception.UnsupportedFileFormatException;
 
 public interface TransactionService {
 
 	public void processAndSaveTransactions(MultipartFile file)
-			throws IOException, InvalidFileNameException, UnsupportedFileFormatException, FileProcessingException;
+			throws IOException, EmptyFileException, UnsupportedFileFormatException;
 
 }
--- transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/service/impl/TransactionServiceImpl.java
+++ transaction-processing-tdd/src/main/java/com/altimetrik/transactionprocessingtdd/service/impl/TransactionServiceImpl.java
@@ -1,12 +1,24 @@
 package com.altimetrik.transactionprocessingtdd.service.impl;
 
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.CSV_DELIMITER;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.CSV_EXTENSION;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.DATE_FORMATTER;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.DATE_FORMATTER_EXCEL;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.EXCEL_EXTENSION;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.FILE_IS_EMPTY;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.FIXEDLENGTHFILEFORMAT_EXTENSION;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.TRANSACTIONTYPE_CARD;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.TRANSACTIONTYPE_WALLET;
+import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.UNSUPPORTED_FILE_FORMAT;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -16,16 +28,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.altimetrik.transactionprocessingtdd.dto.TransactionDto;
-import com.altimetrik.transactionprocessingtdd.exception.FileProcessingException;
-import com.altimetrik.transactionprocessingtdd.exception.InvalidFileNameException;
+import com.altimetrik.transactionprocessingtdd.exception.EmptyFileException;
 import com.altimetrik.transactionprocessingtdd.exception.UnsupportedFileFormatException;
 import com.altimetrik.transactionprocessingtdd.model.CardTransaction;
 import com.altimetrik.transactionprocessingtdd.model.WalletTransaction;
 import com.altimetrik.transactionprocessingtdd.repository.CardTransactionRepository;
 import com.altimetrik.transactionprocessingtdd.repository.WalletTransactionRepository;
 import com.altimetrik.transactionprocessingtdd.service.TransactionService;
-import static com.altimetrik.transactionprocessingtdd.utils.TransactionProcessingConstants.*;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
@@ -38,208 +47,145 @@ public class TransactionServiceImpl implements TransactionService {
 
 	@Override
 	public void processAndSaveTransactions(MultipartFile file)
-			throws IOException, InvalidFileNameException, UnsupportedFileFormatException, FileProcessingException {
+			throws EmptyFileException, UnsupportedFileFormatException, IOException {
+
+		if (file.isEmpty()) {
+			throw new EmptyFileException(FILE_IS_EMPTY);
+		}
+
 		String filename = file.getOriginalFilename();
 		if (filename == null) {
-			throw new InvalidFileNameException(INVALID_FILE_NAME);
+			throw new UnsupportedFileFormatException(UNSUPPORTED_FILE_FORMAT);
 		}
 
-		try {
-			if (filename.endsWith(EXCEL_EXTENSION)) {
-				processExcelFile(file);
-			} else if (filename.endsWith(CSV_EXTENSION)) {
-				processCsvFile(file);
-			} else if (filename.endsWith(FIXEDLENGTHFILEFORMAT_EXTENSION)) {
-				processFixedLengthFile(file);
-			} else {
-				throw new UnsupportedFileFormatException(UNSUPPORTED_FILE_FORMAT);
-			}
-		} catch (FileProcessingException e) {
-			throw new FileProcessingException(FILE_PROCESSING_ERROR + e.getMessage());
+		String fileExtension = getFileExtension(filename);
+		switch (fileExtension) {
+		case EXCEL_EXTENSION:
+			processExcelFile(file);
+			break;
+		case CSV_EXTENSION:
+			processCsvFile(file);
+			break;
+		case FIXEDLENGTHFILEFORMAT_EXTENSION:
+			processFixedLengthFile(file);
+			break;
+		default:
+			throw new UnsupportedFileFormatException(UNSUPPORTED_FILE_FORMAT);
 		}
 
 	}
 
-	private void processExcelFile(MultipartFile file) throws IOException, FileProcessingException {
+	private String getFileExtension(String filename) {
+
+		int extensionIndex = filename.lastIndexOf('.');
+		return (extensionIndex >= 0 && extensionIndex < filename.length() - 1) ? filename.substring(extensionIndex + 1)
+				: "";
+	}
+
+	private void processExcelFile(MultipartFile file) throws IOException {
 		try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
 			Sheet sheet = workbook.getSheetAt(0); // Assuming there is only one sheet
+			Stream<Row> rowStream = StreamSupport.stream(sheet.spliterator(), false);
 
-			boolean skipHeader = true; // Flag to skip the first row (header)
+			rowStream.skip(1).forEach(this::saveTransactionFromRow);
 
-			for (Row row : sheet) {
-				if (skipHeader) {
-					skipHeader = false;
-					continue; // Skip the first row
-				}
-
-				TransactionDto transaction = getTransactionFromRow(row);
-				saveTransaction(transaction);
-			}
-		} catch (IOException e) {
-			throw new FileProcessingException();
 		}
 	}
 
-	private void processCsvFile(MultipartFile file) throws IOException, FileProcessingException {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-			List<String> lines = reader.lines().skip(1).collect(Collectors.toList()); // Skip the header line
+	private void saveTransactionFromRow(Row row) {
+		String transactionId = getCellValue(row, 0);
+		String transactionType = getCellValue(row, 1);
+		double amount = Double.parseDouble(getCellValue(row, 5));
+		String remarks = getCellValue(row, 6);
 
-			for (String line : lines) {
-				String[] fields = line.split(CSV_DELIMITER); // Assuming fields are
-																// comma-separated
-				TransactionDto transaction = getTransactionFromFields(fields);
-				saveTransaction(transaction);
-			}
-		} catch (IOException e) {
-			throw new FileProcessingException(FILE_PROCESSING_ERROR + e.getMessage());
+		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transactionType)) {
+			String cardNumber = getCellValue(row, 2);
+			LocalDate expiryDate = LocalDate.parse(getCellValue(row, 3), DATE_FORMATTER_EXCEL);
+			int cvv = Integer.parseInt(getCellValue(row, 4));
+
+			cardTransactionRepository
+					.save(new CardTransaction(transactionId, cardNumber, expiryDate, cvv, amount, remarks));
+		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transactionType)) {
+			String walletType = getCellValue(row, 2);
+			String upiId = getCellValue(row, 3);
+			double balance = Double.parseDouble(getCellValue(row, 4));
+
+			walletTransactionRepository
+					.save(new WalletTransaction(transactionId, walletType, upiId, balance, amount, remarks));
 		}
 	}
 
-	private void processFixedLengthFile(MultipartFile file) throws IOException, FileProcessingException {
-		try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
-			List<String> lines = reader.lines().skip(1).collect(Collectors.toList()); // Skip the header line
-
-			for (String line : lines) {
-				TransactionDto transaction = getTransactionFromFixedLengthFile(line);
-				saveTransaction(transaction);
-			}
-		} catch (IOException e) {
-			throw new FileProcessingException(FILE_PROCESSING_ERROR + e.getMessage());
-		}
-	}
-
-	private TransactionDto getTransactionFromRow(Row row) {
-
+	private String getCellValue(Row row, int index) {
 		DataFormatter dataFormatter = new DataFormatter();
-		TransactionDto transaction = new TransactionDto();
-
-		String transactionId = dataFormatter.formatCellValue(row.getCell(0));
-		String transactionType = dataFormatter.formatCellValue(row.getCell(1));
-		double amount = Double.parseDouble(dataFormatter.formatCellValue(row.getCell(5)));
-		String remarks = dataFormatter.formatCellValue(row.getCell(6));
-
-		transaction.setTransactionId(transactionId);
-		transaction.setTransactionType(transactionType);
-		transaction.setAmount(amount);
-		transaction.setRemarks(remarks);
-
-		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transaction.getTransactionType())) {
-
-			String cardNumber = dataFormatter.formatCellValue(row.getCell(2));
-			LocalDate expiryDate = LocalDate.parse(dataFormatter.formatCellValue(row.getCell(3)), DATE_FORMATTER_EXCEL);
-			int cvv = Integer.parseInt(dataFormatter.formatCellValue(row.getCell(4)));
-
-			transaction.setCardNumber(cardNumber);
-			transaction.setExpiryDate(expiryDate);
-			transaction.setCvv(cvv);
-		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transaction.getTransactionType())) {
-
-			String walletType = dataFormatter.formatCellValue(row.getCell(2));
-			String upiId = dataFormatter.formatCellValue(row.getCell(3));
-			double balance = Double.parseDouble(dataFormatter.formatCellValue(row.getCell(4)));
-
-			transaction.setWalletType(walletType);
-			transaction.setUpiId(upiId);
-			transaction.setBalance(balance);
-		}
-
-		return transaction;
+		Cell cell = row.getCell(index);
+		return dataFormatter.formatCellValue(cell);
 	}
 
-	private TransactionDto getTransactionFromFields(String[] fields) {
+	private void processCsvFile(MultipartFile file) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
-		TransactionDto transaction = new TransactionDto();
+			reader.lines().skip(1).forEach(this::saveTransactionFromCSV);
+
+		}
+	}
+
+	private void saveTransactionFromCSV(String record) {
+
+		String[] fields = record.split(CSV_DELIMITER);
 
 		String transactionId = fields[0];
 		String transactionType = fields[1];
 		double amount = Double.parseDouble(fields[5]);
 		String remarks = fields[6];
 
-		transaction.setTransactionId(transactionId);
-		transaction.setTransactionType(transactionType);
-		transaction.setAmount(amount);
-		transaction.setRemarks(remarks);
-
-		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transaction.getTransactionType())) {
+		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transactionType)) {
 
 			String cardNumber = fields[2];
 			LocalDate expiryDate = LocalDate.parse(fields[3], DATE_FORMATTER);
 			int cvv = Integer.parseInt(fields[4]);
 
-			transaction.setCardNumber(cardNumber);
-			transaction.setExpiryDate(expiryDate);
-			transaction.setCvv(cvv);
-
-		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transaction.getTransactionType())) {
+			cardTransactionRepository
+					.save(new CardTransaction(transactionId, cardNumber, expiryDate, cvv, amount, remarks));
+		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transactionType)) {
 
 			String walletType = fields[2];
 			String upiId = fields[3];
 			double balance = Double.parseDouble(fields[4]);
 
-			transaction.setWalletType(walletType);
-			transaction.setUpiId(upiId);
-			transaction.setBalance(balance);
-
+			walletTransactionRepository
+					.save(new WalletTransaction(transactionId, walletType, upiId, balance, amount, remarks));
 		}
-
-		return transaction;
 	}
 
-	private TransactionDto getTransactionFromFixedLengthFile(String line) {
-		TransactionDto transaction = new TransactionDto();
+	private void processFixedLengthFile(MultipartFile file) throws IOException {
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
 
-		String transactionId = line.substring(0, 10).trim();
-		String transactionType = line.substring(11, 26).trim();
-		double amount = Double.parseDouble(line.substring(55, 67).trim());
-		String remarks = line.substring(67).trim();
-
-		transaction.setTransactionId(transactionId);
-		transaction.setTransactionType(transactionType);
-		transaction.setAmount(amount);
-		transaction.setRemarks(remarks);
-
-		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transaction.getTransactionType())) {
-
-			String cardNumber = line.substring(26, 39).trim();
-			int cvv = Integer.parseInt(line.substring(40, 43).trim());
-			LocalDate expiryDate = LocalDate.parse(line.substring(44, 54).trim(), DATE_FORMATTER);
-
-			transaction.setCardNumber(cardNumber);
-			transaction.setExpiryDate(expiryDate);
-			transaction.setCvv(cvv);
-
-		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transaction.getTransactionType())) {
-
-			String walletType = line.substring(26, 39).trim();
-			String upiId = line.substring(40, 43).trim();
-			double balance = Double.parseDouble(line.substring(44, 54).trim());
-
-			transaction.setWalletType(walletType);
-			transaction.setUpiId(upiId);
-			transaction.setBalance(balance);
+			reader.lines().skip(1).forEach(this::saveTransactionFromFixedLengthFile);
 
 		}
-
-		return transaction;
 	}
 
-	private void saveTransaction(TransactionDto transaction) {
+	private void saveTransactionFromFixedLengthFile(String record) {
+		String transactionId = record.substring(0, 10).trim();
+		String transactionType = record.substring(11, 26).trim();
+		double amount = Double.parseDouble(record.substring(55, 67).trim());
+		String remarks = record.substring(67).trim();
 
-		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transaction.getTransactionType())) {
+		if (TRANSACTIONTYPE_CARD.equalsIgnoreCase(transactionType)) {
+			String cardNumber = record.substring(26, 39).trim();
+			int cvv = Integer.parseInt(record.substring(40, 43).trim());
+			LocalDate expiryDate = LocalDate.parse(record.substring(44, 54).trim(), DATE_FORMATTER);
 
-			CardTransaction newTransaction = new CardTransaction(transaction.getTransactionId(),
-					transaction.getCardNumber(), transaction.getExpiryDate(), transaction.getCvv(),
-					transaction.getAmount(), transaction.getRemarks());
-			cardTransactionRepository.save(newTransaction);
+			cardTransactionRepository
+					.save(new CardTransaction(transactionId, cardNumber, expiryDate, cvv, amount, remarks));
+		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transactionType)) {
+			String walletType = record.substring(26, 39).trim();
+			String upiId = record.substring(40, 43).trim();
+			double balance = Double.parseDouble(record.substring(44, 54).trim());
 
-		} else if (TRANSACTIONTYPE_WALLET.equalsIgnoreCase(transaction.getTransactionType())) {
-
-			WalletTransaction newTransaction = new WalletTransaction(transaction.getTransactionId(),
-					transaction.getWalletType(), transaction.getUpiId(), transaction.getBalance(),
-					transaction.getAmount(), transaction.getRemarks());
-			walletTransactionRepository.save(newTransaction);
+			walletTransactionRepository
+					.save(new WalletTransaction(transactionId, walletType, upiId, balance, amount, remarks));
 		}
-		System.out.println(transaction);
 	}
 
 }
